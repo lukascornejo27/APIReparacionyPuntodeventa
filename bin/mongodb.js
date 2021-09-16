eef3b6ab09a3a7339faeaa038b43d494867d95c8
lukascornejo27 <lukascornejo27@gmail.com>
2021-09-16 13:29:34 -0300

    Update mongodb.js

--- bin/mongodb.js
+++ bin/mongodb.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-//const uri = "mongodb+srv://lukascornejo27:lara1015@cluster0.qwt5j.mongodb.net/helpdb?retryWrites=true&w=majority";
-const uri2 = 'mongodb://localhost:27017/repSalesDB';
+const uri = "mongodb+srv://lukascornejo27:lara1015@cluster0.qwt5j.mongodb.net/apirepventdbdemo?retryWrites=true&w=majority";
+//const uri = 'mongodb://localhost:27017/repSalesDB';
 try {
     // Connect to the MongoDB cluster
      mongoose.connect(
-      uri2,
+      uri,
       { useNewUrlParser: true, useUnifiedTopology: true },
       () => console.log(" Mongoose is connected")
     );
